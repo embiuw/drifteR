a25e604ee8063b3ed7bf861688d409d63ac29626
unknown <Administrator@HI-09317.imr.no>
2020-04-30 23:17:22 +0200

    Small correction in model path

--- R/tfClassifier.R
+++ R/tfClassifier.R
@@ -1,7 +1,9 @@
 tfClassifier <- function(data) {
   require(keras)
-  model <- load_model_hdf5('diveClassTF.h5')
-  system.file("extdata", "diveClassTF.h5", package = "drifteR", mustWork = TRUE)
+
+  mod.nam <- system.file("extdata", "diveClassTF.h5", package = "drifteR", mustWork = TRUE)
+  model <- load_model_hdf5(mod.nam)
+
   if(dat$N.DEPTHS==5) dat <- strip.last(data)
 
   dat <- standardise.dive(dat)
