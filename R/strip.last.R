0b605946efc3546ca630c51df5c6c3a0b9576d75
unknown <Administrator@HI-09317.imr.no>
2020-04-30 23:43:17 +0200

    Corrected small error

--- R/strip.last.R
+++ R/strip.last.R
@@ -1,9 +1,10 @@
 strip.last <- function(dat=hp6) {
   ## Must have run 'rbs' first!
+  orig.names <- c(paste0('T', c(1:5)), paste0('D', c(1:5)))
   new.dat <- matrix(NA, nrow=nrow(dat), ncol=8)
   pb <- txtProgressBar(title = "Reorganising remaining inflection point data", min = 0,
                        max = nrow(dat), style=3)
-  
+
   for(i in 1:nrow(dat)) {
     setTxtProgressBar(pb, i, title=paste("Reorganising remaining inflection point data",
                                          round(i/nrow(dat)*100, 0),
@@ -12,9 +13,9 @@ strip.last <- function(dat=hp6) {
       last <- tail(unlist(strsplit(dat$order[i], '.', fixed=T)), 1)
       keep.names <- setdiff(orig.names, paste0(c('T', 'D'), last))
       new.dat[i,] <- as.numeric(dat[i,match(keep.names, names(dat))])
-    }  
+    }
   }
-  
+
   close(pb)
   new.dat <- as.data.frame(new.dat)
   names(new.dat) <- c(paste0('T', c(1:4)), paste0('D', c(1:4)))
