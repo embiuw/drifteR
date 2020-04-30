e51ee53b39a554459c12f468687c6634b24ea287
unknown <Administrator@HI-09317.imr.no>
2020-04-30 23:14:00 +0200

    Changed model dependency

--- R/tfClassifier.R
+++ R/tfClassifier.R
@@ -1,7 +1,7 @@
 tfClassifier <- function(data) {
   require(keras)
   model <- load_model_hdf5('diveClassTF.h5')
-
+  system.file("extdata", "diveClassTF.h5", package = "drifteR", mustWork = TRUE)
   if(dat$N.DEPTHS==5) dat <- strip.last(data)
 
   dat <- standardise.dive(dat)
