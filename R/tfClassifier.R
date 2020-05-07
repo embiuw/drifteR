8d35ae17b618c7e811e5d97fd179c05cf3f3854a
unknown <Administrator@HI-09317.imr.no>
2020-05-07 11:27:14 +0200

    Changed error with n.bs

--- R/tfClassifier.R
+++ R/tfClassifier.R
@@ -1,15 +1,27 @@
 #' @export
 
-tfClassifier <- function(data) {
+tfClassifier <- function(data, n.bs=NA) {
   require(keras)
 
   mod.nam <- system.file("extdata", "diveClassTF.h5", package = "drifteR", mustWork = TRUE)
   model <- load_model_hdf5(mod.nam)
 
-  if(data$N.DEPTHS[1]==5) {
-    dat <- strip.last(data)
+  if('N.DEPTHS' %in% names(data)) {
+    if(data$N.DEPTHS[1]==5) {
+      dat <- strip.last(data)
+    } else {
+      dat <- data
+    }
   } else {
-    dat <- data
+    if(is.na(n.bs)) {
+      stop('no input provided to n.bbs. Please specify number of inflection points in data')
+    } else {
+      if(n.bs==5) {
+        dat <- strip.last(data)
+      } else {
+        dat <- data
+      }
+    }
   }
 
   dat <- standardise.dive(dat)
